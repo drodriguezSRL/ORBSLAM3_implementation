8539241ef719f48a15ae126e44047a3b3cbc3b57
drodriguez <davidrm.research@gmail.com>
2025-05-13 17:12:44 +0200

    improved tool rename_data

--- tools/rename_data.py
+++ tools/rename_data.py
@@ -2,26 +2,40 @@
 
 import os
 import re
+import argparse
 
-data_folder_path = ""
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Rename files in a directory based on a timestamp in the filename.")
+    parser.add_argument("data_folder_path", type=str, help="Path to the folder containing the files to rename.")
+    return parser.parse_args()
 
-name_pattern = re.compile(r"stereo\_(\w+)\_(\d+\.\d+)\_0")
+def main():
+    args = parse_arguments()
 
-for filename in os.listdir(data_folder_path):
-    match = name_pattern.match(filename)
-    if match:
-        timestamp_str = match.group(1) # extract timestamp
-        try:
-            timestamp_ns = int(float(timestamp_str) * 1e9) # convert to nanoseconds
-            new_filename = f"{timestamp_ns}.png"
+    data_folder_path = args.data_folder_path
+    if not os.path.isdir(data_folder_path):
+        print(f"Error: The path {data_folder_path} is not a valid directory.")
+        return
 
-            old_file_path = os.path.join(data_folder_path, filename)
-            new_file_path = os.path.join(data_folder_path, new_filename)
+    name_pattern = re.compile(r"stereo\_(\w+)\_(\d+\.\d+)\_0")
 
-            os.rename(old_file_path, new_file_path)
+    for filename in os.listdir(data_folder_path):
+        match = name_pattern.match(filename)
+        if match:
+            timestamp_str = match.group(1) # extract timestamp
+            try:
+                timestamp_ns = int(float(timestamp_str) * 1e9) # convert to nanoseconds
+                new_filename = f"{timestamp_ns}.png"
 
-        except ValueError:
-            print(f"Error converting timestamp: {timestamp_str} in file {filename}")
-    else:
-        print(f"Filename {filename} does not match the expected pattern.")
+                old_file_path = os.path.join(data_folder_path, filename)
+                new_file_path = os.path.join(data_folder_path, new_filename)
 
+                os.rename(old_file_path, new_file_path)
+
+            except ValueError:
+                print(f"Error converting timestamp: {timestamp_str} in file {filename}")
+        else:
+            print(f"Filename {filename} does not match the expected pattern.")
+
+if __name__ == "__main__":
+    main()

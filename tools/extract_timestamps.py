cb750e3b6680663a7a934d4ad8b8cdf87455577b
drodriguez <davidrm.research@gmail.com>
2025-06-03 15:32:32 +0200

    arguments to extract_timestamps

--- tools/extract_timestamps.py
+++ tools/extract_timestamps.py
@@ -1,4 +1,13 @@
+import os
+import re
 import csv
+import argparse
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Generate a TXT file from a CSV file.")
+    parser.add_argument("csv_folder_path", type=str, help="Path to the CSV file.")
+    parser.add_argument("output_txt_path", type=str, help="Path to the output TXT file.")
+    return parser.parse_args()
 
 def extract_timestamps(input_file, output_file):
     with open(input_file, 'r', newline='') as infile, open(output_file, 'w', newline='') as outfile:
@@ -11,5 +20,22 @@ def extract_timestamps(input_file, output_file):
             parts = line.strip().split(',')
             if parts:
                 outfile.write(parts[0] + '\n')
+    
 
-extract_timestamps('/mnt/c/users/david/downloads/spice-hl3/cam0/data.csv', '/mnt/c/users/david/downloads/spice-hl3/spicehl3_timeStamps/trajectory_F.txt')
+def main():
+    args = parse_arguments()
+
+    csv_file = args.csv_folder_path
+    txt_file = args.output_txt_path
+
+    if not os.path.isfile(csv_file):
+        print(f"Error: The path {csv_file} is not a valid file.")
+        return
+
+    extract_timestamps(csv_file, txt_file)  
+    print(f"Timestamps txt file saved to: {txt_file}")  
+
+
+if __name__ == "__main__":
+    main()
+
